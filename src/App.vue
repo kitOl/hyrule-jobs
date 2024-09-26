c2e74ade230d5b6d6399a5ff5934de9305b9667c
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-09-27 02:03:16 +0300

    added JobList

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <template>
-  <div class="app">Hello, hyrule</div>
+  <div class="app">
+    <JobsList :jobs="jobs" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import JobsList from './components/JobsList.vue';
+import Job from './types/Job';
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: { JobsList },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon ranch', salary: 30000, id: '1' },
+      {
+        title: 'quarryman',
+        location: 'death mountain',
+        salary: 40000,
+        id: '2',
+      },
+      {
+        title: 'flute player',
+        location: 'the lost woods',
+        salary: 35000,
+        id: '3',
+      },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      {
+        title: 'prison guard',
+        location: 'gerudo valley',
+        salary: 32000,
+        id: '5',
+      },
+    ]);
+
+    return { jobs };
+  },
 });
 </script>
 
