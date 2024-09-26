354c7db8cd7fdfd570ee7ec2a8cf0990a8b1703c
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-09-27 02:55:34 +0300

    added sorting posts by filter

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="job-list">
+    <p>Ordered by {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }} rupees</p>
@@ -20,8 +21,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType } from 'vue';
 import Job from '@/types/Job';
+import OrderTerm from '@/types/OrderTerm';
 
 export default defineComponent({
   props: {
@@ -29,6 +31,19 @@ export default defineComponent({
       required: true,
       type: Array as PropType<Job[]>,
     },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>,
+    },
+  },
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+
+    return { orderedJobs };
   },
 });
 </script>
